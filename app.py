f4d2043d3e8810ebffe347a562b92bd96eb3f739
dannigalvez <danielgalvez@simnet.is>
2018-04-29 18:34:33 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from bottle import *
+import urllib.request, json, os
+
+with urllib.request.urlopen("http://apis.is/petrol") as url:
+    data = json.loads(url.read().decode())
+
+@route('/static/<img>')
+def img(img):
+    return static_file(img, root='./')
+
+@route('/')
+def index():
+    return template('index',data=data)
+
+@route('/company/<company>')
+def index(company):
+    return template('company',data=data,c=company)
+
+@route('/moreinfo/<key>')
+def index(key):
+    return template('moreinfo',data=data,k=key)
+
+run(debug=True)
